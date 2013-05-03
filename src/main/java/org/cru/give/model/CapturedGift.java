9c265b7f4e7a1e1c7e9c1bc8dd123b67c0a2a299
Ryan Carlson <ryan.t.carlson@cru.org>
2013-05-03 13:59:56 -0400

    Creating gift jpa/db model

--- src/main/java/org/cru/give/model/CapturedGift.java
+++ src/main/java/org/cru/give/model/CapturedGift.java
@@ -4,122 +4,100 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
-import com.google.api.client.util.Key;
-
-//@Entity
-//@Table(name="captured_gift_cart_gift")
-public class CapturedGift
+@Entity
+@Table(name="captured_gift_cart_gift")
+public class CapturedGift implements java.io.Serializable
 {
-    @Key
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(generator = "gift_seq", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name="gift_seq", sequenceName = "cru_gift_seq", allocationSize=1)
+	@Column(name = "GIFT_ID")
+	private Long giftId;
+	@Column(name = "CREATED")
+	private Date created;
+	
+	/**
+	 * Foreign keys
+	 */
+	@Column(name = "CART_ID")
+	private Long cartId;
+	@Column(name = "CHECKOUT_ID")
     private String checkoutId;
-    @Key
-    private String designationId;
-    
+	@Column(name = "QUICK_ENTRY_ID")
     private String quickEntryId;
-//	private USDollar amount;
-	
-    @Column(name="CAMPAIGN_ID") /*we're really saving the campaign code here, not the row_id*/
-	private String campaignCode;
-	@Key
-	private String giftDay;
-	@Key
-	private String giftFrequency;
-	private Date startDate;
-	private String motivation;
-	
-	private String commentToDonationServices;
-	private String commentToStaff;
+    
+    /**
+     * Required Gift information fields
+     */
 	
 	/**
 	 * "Donation" or "Recurring"
 	 */
-	private String orderType;
+	@Column(name = "ORDER_TYPE")
+    private String orderType;
+	@Column(name = "DESIGNATION_ID")
+    private String designationId;
+	@Column(name = "AMOUNT")
+    private Double amount;
+	@Column(name = "GIFT_DAY")
+    private String giftDay;
+	@Column(name = "GIFT_FREQUENCY")
+	private String giftFrequency;
+	@Column(name = "START_DATE")
+	private Date startDate;
 	
-	
-	private Date created;
-	
-	public CapturedGift()
+	/**
+	 * Optional marketing fields
+	 */
+    @Column(name = "CAMPAIGN_ID") /*we're really saving the campaign code here, not the row_id*/
+	private String campaignCode;
+    @Column(name = "MOTIVATION")
+    private String motivation;
+    
+	/**
+	 * Optional comments from gift-giver
+	 */
+    @Column(name = "COMMENT_TO_DONATION_SERVICES")
+	private String commentToDonationServices;
+    @Column(name = "COMMENT_TO_STAFF")
+    private String commentToStaff;
+
+	public Long getGiftId()
 	{
+		return giftId;
 	}
-	
-	public CapturedGift(String checkoutId)
+
+	public void setGiftId(Long giftId)
 	{
-		this.checkoutId = checkoutId;
+		this.giftId = giftId;
 	}
-	
-	public String getQuickEntryId()
-	{
-		return quickEntryId;
-	}
-	public void setQuickEntryId(String rowId)
-	{
-		this.quickEntryId = rowId;
-	}
-//	public USDollar getAmount()
-//	{
-//		return amount;
-//	}
-//	public void setAmount(USDollar amount)
-//	{
-//		this.amount = amount;
-//	}
-	public String getCampaignCode()
-	{
-		return campaignCode;
-	}
-	public void setCampaignCode(String campaignCode)
-	{
-		this.campaignCode = campaignCode;
-	}
-	public String getGiftDay()
-	{
-		return giftDay;
-	}
-	public void setGiftDay(String giftDay)
-	{
-		this.giftDay = giftDay;
-	}
-	public String getGiftFrequency()
-	{
-		return giftFrequency;
-	}
-	public void setGiftFrequency(String giftFreq)
-	{
-		this.giftFrequency = giftFreq;
-	}
-	public Date getStartDate()
-	{
-		return startDate;
-	}
-	public void setStartDate(Date startDt)
-	{
-		this.startDate = startDt;
-	}
-	public String getDesignationId()
-	{
-		return designationId;
-	}
-	public void setDesignationId(String desigId)
-	{
-		this.designationId = desigId;
-	}
-	public String getOrderType()
-	{
-		return orderType;
-	}
-	public void setOrderType(String orderType)
-	{
-		this.orderType = orderType;
-	}
+
 	public Date getCreated()
 	{
 		return created;
 	}
+
 	public void setCreated(Date created)
 	{
 		this.created = created;
+	}
+
+	public Long getCartId()
+	{
+		return cartId;
+	}
+
+	public void setCartId(Long cartId)
+	{
+		this.cartId = cartId;
 	}
 
 	public String getCheckoutId()
@@ -132,25 +110,75 @@ public class CapturedGift
 		this.checkoutId = checkoutId;
 	}
 
-    public String getCommentToDonationServices()
-    {
-        return commentToDonationServices;
-    }
+	public String getQuickEntryId()
+	{
+		return quickEntryId;
+	}
 
-    public void setCommentToDonationServices(String commentToDonationServices)
-    {
-        this.commentToDonationServices = commentToDonationServices;
-    }
+	public void setQuickEntryId(String quickEntryId)
+	{
+		this.quickEntryId = quickEntryId;
+	}
 
-    public String getCommentToStaff()
-    {
-        return commentToStaff;
-    }
+	public String getDesignationId()
+	{
+		return designationId;
+	}
 
-    public void setCommentToStaff(String commentToStaff)
-    {
-        this.commentToStaff = commentToStaff;
-    }
+	public void setDesignationId(String designationId)
+	{
+		this.designationId = designationId;
+	}
+
+	public Double getAmount()
+	{
+		return amount;
+	}
+
+	public void setAmount(Double amount)
+	{
+		this.amount = amount;
+	}
+
+	public String getGiftDay()
+	{
+		return giftDay;
+	}
+
+	public void setGiftDay(String giftDay)
+	{
+		this.giftDay = giftDay;
+	}
+
+	public String getGiftFrequency()
+	{
+		return giftFrequency;
+	}
+
+	public void setGiftFrequency(String giftFrequency)
+	{
+		this.giftFrequency = giftFrequency;
+	}
+
+	public Date getStartDate()
+	{
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate)
+	{
+		this.startDate = startDate;
+	}
+
+	public String getCampaignCode()
+	{
+		return campaignCode;
+	}
+
+	public void setCampaignCode(String campaignCode)
+	{
+		this.campaignCode = campaignCode;
+	}
 
 	public String getMotivation()
 	{
@@ -160,5 +188,35 @@ public class CapturedGift
 	public void setMotivation(String motivation)
 	{
 		this.motivation = motivation;
+	}
+
+	public String getCommentToDonationServices()
+	{
+		return commentToDonationServices;
+	}
+
+	public void setCommentToDonationServices(String commentToDonationServices)
+	{
+		this.commentToDonationServices = commentToDonationServices;
+	}
+
+	public String getCommentToStaff()
+	{
+		return commentToStaff;
+	}
+
+	public void setCommentToStaff(String commentToStaff)
+	{
+		this.commentToStaff = commentToStaff;
+	}
+
+	public String getOrderType()
+	{
+		return orderType;
+	}
+
+	public void setOrderType(String orderType)
+	{
+		this.orderType = orderType;
 	}
 }
