837d8bf81994bc77c2cc027616b605a1bf63240d
Ryan Carlson <ryan.t.carlson@cru.org>
2013-05-03 14:06:05 -0400

    Build out Post, Put and Delete methods on Gift resource

--- src/main/java/org/cru/give/webservices/GiftResource.java
+++ src/main/java/org/cru/give/webservices/GiftResource.java
@@ -2,7 +2,11 @@ package org.cru.give.webservices;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -25,4 +29,27 @@ public class GiftResource
 	{
 		return giftService.fetchGift(giftId);
 	}
+	
+	@POST
+	@Path("/")
+	public void createGift()
+	{
+		
+	}
+	
+	@DELETE
+	@Path("/{giftId}")
+	public void deleteGift(@PathParam("giftId") String giftId)
+	{
+		
+	}
+	
+	@PUT
+	@Path("/{giftId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void updateCart(@PathParam("giftId") String giftId, GiftDetails gift)
+	{
+		
+	}
+	
 }
