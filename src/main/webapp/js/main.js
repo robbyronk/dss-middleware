2fea381e237d198eb3f2b818b908ba9589b10a72
Ryan Carlson <ryan.t.carlson@cru.org>
2013-05-08 09:40:58 -0400

    Add files to run a test DSS page to hit the endpoint from that side too

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,5 +1,9 @@
-function GiftCartController($scope){
-	$scope.submitGiftCart = function(){
-		alert('Cart Submitted');
+function GiftCartController($scope, $http){
+
+	$scope.saveGift = function(){
+		alert('Gift Saved');
+		$scope.gift.giftId = 1;
+		$http.put('http://localhost:8080/dss-middleware/rest/gift', $scope.gift);
 	};
-}
+	
+};
