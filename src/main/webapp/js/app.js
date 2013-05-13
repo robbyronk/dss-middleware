f5c18f687317d0843b5e7a342bb23be3429a1b4d
Bill Randall <william.randall@cru.org>
2013-05-13 17:47:54 -0400

    Put controller in the javascript instead of html so we can have multiple views

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -4,7 +4,8 @@ angular.module('dssMiddlewareApp', [])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/GiftDetail.html'
+        templateUrl: 'views/GiftDetail.html',
+        controller: 'GiftDetlController'
       })
       .otherwise({
         redirectTo: '/'
