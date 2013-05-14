c6e4600920e5a20eb14e649ad0fb8d6bcb674757
Bill Randall <william.randall@cru.org>
2013-05-14 15:20:39 -0400

    Added comment functionality

--- src/main/webapp/js/controllers/giftDetl.js
+++ src/main/webapp/js/controllers/giftDetl.js
@@ -6,6 +6,8 @@ angular.module('dssMiddlewareApp')
 		$scope.params = $routeParams;
 		$scope.designationNumber = $scope.params.designationNumber;
 		$scope.frequencies = ['Single','Monthly','Quarterly','Semi-Annual','Annual'];
+		$scope.showDonComment = 'N';
+		$scope.showStaffComment = 'N';
 		
 		/*creates a new 'blank cart' in the database and looks for the URI to 
 		 *the resource in the header 'Location'*/
@@ -46,5 +48,24 @@ angular.module('dssMiddlewareApp')
 			$scope.gift.cartId = $scope.cart.cartId;
 			
 			$http.put('http://localhost:8080/dss-middleware/rest/gift', $scope.gift);
+		};
+		
+		//TODO: should be a service
+		$scope.isMaxLength = function(obj) {
+			var mlength = obj.getAttribute?parseInt(obj.getAttribute("maxlength")) : "";
+			if (obj.getAttribute && obj.value.length > mlength) {
+				obj.value = obj.value.substring(0,mlength);
+			}
+		};
+		
+		$scope.toggleShowComment = function(type, $event) {
+			$event.preventDefault();
+			
+			if(type == 'don') {
+				$scope.showDonComment = $scope.showDonComment == 'Y'?'N': 'Y';
+			}
+			else if(type == 'staff') {
+				$scope.showStaffComment = $scope.showStaffComment == 'Y'?'N': 'Y';
+			}
 		};
 });
