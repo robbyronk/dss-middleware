3af98f19c22b4480719101380d933324822271b9
Bill Randall <william.randall@cru.org>
2013-05-14 10:04:22 -0400

    Add a way to get to the GiftCartPage

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -7,6 +7,10 @@ angular.module('dssMiddlewareApp', [])
         templateUrl: 'views/GiftDetail.html',
         controller: 'GiftDetlController'
       })
+      .when('/GiftCartPage', {
+    	  templateUrl: 'views/GiftCartPage.html',
+    	  controller: 'GiftCartController'
+      })
       .otherwise({
         redirectTo: '/GiftDetail'
       });
