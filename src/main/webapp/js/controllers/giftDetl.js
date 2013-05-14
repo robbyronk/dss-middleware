1eead1e84d17f9299af99051e568e54b4e6823f6
Bill Randall <william.randall@cru.org>
2013-05-14 10:06:42 -0400

    Put a space before the curly braces for more readability

--- src/main/webapp/js/controllers/giftDetl.js
+++ src/main/webapp/js/controllers/giftDetl.js
@@ -1,17 +1,17 @@
 'use strict';
 
 angular.module('dssMiddlewareApp')
-	.controller('GiftDetlController', function ($scope, $http){
+	.controller('GiftDetlController', function ($scope, $http) {
 	
 	/*creates a new 'blank cart' in the database and looks for the URI to 
 	 *the resource in the header 'Location'*/
-	$scope.createCart = function(){
+	$scope.createCart = function() {
 		$http.post('http://localhost:8080/dss-middleware/rest/cart')
-			.success(function(data, status, headers, config){
+			.success(function(data, status, headers, config) {
 				var createdCartLocation = headers('Location');
 			
 				$http.get(createdCartLocation)
-					.success(function(data){
+					.success(function(data) {
 						$scope.cart = data;
 					});
 			});
@@ -19,13 +19,13 @@ angular.module('dssMiddlewareApp')
 	
 	/*creates a new 'blank gift' in the database and looks for the URI to 
 	 *the resource in the header 'Location'*/
-	$scope.createGift = function(){
+	$scope.createGift = function() {
 		$http.post('http://localhost:8080/dss-middleware/rest/gift')
-			.success(function(data, status, headers, config){
+			.success(function(data, status, headers, config) {
 				var createdGiftLocation = headers('Location');
 
 				$http.get(createdGiftLocation)
-					.success(function(data){
+					.success(function(data) {
 						$scope.gift = data;
 						
 						/*for now we assume there is no cart and it must be created*/
@@ -35,7 +35,7 @@ angular.module('dssMiddlewareApp')
 	};
 	
 	/*update the previously saved gift with the values input by the client*/
-	$scope.saveGift = function(){
+	$scope.saveGift = function() {
 		$scope.gift.cartId = $scope.cart.cartId;
 		
 		$http.put('http://localhost:8080/dss-middleware/rest/gift', $scope.gift);
