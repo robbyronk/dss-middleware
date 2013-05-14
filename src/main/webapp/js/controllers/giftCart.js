82915ab42f05859e7c60547e6f18281156d5e8aa
Bill Randall <william.randall@cru.org>
2013-05-14 10:39:43 -0400

    Removed the excess common html from GiftCartPage. Also added a form submit placeholder

--- src/main/webapp/js/controllers/giftCart.js
+++ src/main/webapp/js/controllers/giftCart.js
@@ -1,12 +1,16 @@
 'use strict';
 
-//TBD below....
 angular.module('dssMiddlewareApp')
-	.controller('GiftCartController', function ($scope, $http){
-	$scope.retrieveCart = function(){
-		$http.get('http://localhost:8080/dss-middleware/rest/cart/')
-			.success(function(data, status, headers, config){
-				$scope.cart = data;
-			});
-	};
+	.controller('GiftCartController', function ($scope, $http) {
+		
+		$scope.retrieveCart = function(cartId) {
+			$http.get('http://localhost:8080/dss-middleware/rest/cart/' + cartId)
+				.success(function(data, status, headers, config){
+					$scope.cart = data;
+				});
+		};
+		
+		$scope.proceedToCheckout = function() {
+			//do stuff
+		};
 });
