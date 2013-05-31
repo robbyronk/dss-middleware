ce925197a05a7f364b7d318c2ebd7fd6f6c04d5c
Bill Randall <william.randall@cru.org>
2013-05-31 11:20:11 -0400

    Re-implemented tests

--- src/test/webapp/js/controllers/giftDetailCtrlTest.js
+++ src/test/webapp/js/controllers/giftDetailCtrlTest.js
@@ -1,10 +1,12 @@
 'use strict';
 
 describe('Gift detail controller tests', function() {
-	describe('Truth', function() {
+	describe('Start date', function() {
 		var scope;
 		var http;
 		var params = {designationNumber: '2843160'};
+		var controlDate;
+		var startDate;
 		
 		beforeEach(module('dssMiddlewareApp'));
 		
@@ -14,8 +16,29 @@ describe('Gift detail controller tests', function() {
 			$controller('GiftDetailCtrl', {$scope: scope, $http: http, $routeParams: params});
 		}));
 		
-		it('Should be true', function() {
-			expect(true).toEqual(true);
+		it('should be May 5th, 2013', function() {
+			controlDate = new Date();
+			controlDate.setFullYear(2013, 4, 5);
+			removeTime(controlDate);
+			startDate = scope.createStartDate(5, 2013, 5);
+			removeTime(startDate);
+			expect(startDate).toEqual(controlDate);
 		});
+		
+		it('should be January 20th, 2014', function() {
+			controlDate = new Date();
+			controlDate.setFullYear(2014, 0, 20);
+			removeTime(controlDate);
+			startDate = scope.createStartDate(1, 2014, 20);
+			removeTime(startDate);
+			expect(startDate).toEqual(controlDate);
+		});
+		
+		function removeTime(date) {
+			date.setHours(0);
+			date.setMinutes(0);
+			date.setSeconds(0);
+			date.setMilliseconds(0);
+		}
 	});
 });
