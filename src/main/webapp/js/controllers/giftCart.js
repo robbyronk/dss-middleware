74ed67bf5720a731b1ed09dba45af7f4e6492f61
Bill Randall <william.randall@cru.org>
2013-06-04 10:51:41 -0400

    Removed WebMacro

--- src/main/webapp/js/controllers/giftCart.js
+++ src/main/webapp/js/controllers/giftCart.js
@@ -3,6 +3,9 @@
 angular.module('dssMiddlewareApp')
 	.controller('GiftCartCtrl', function ($scope, cart) {
 		
+		$scope.giftLines = [{webTitle: 'Ryan T. Carlson', desigType: 'Ministry', designation: '2843160', amount: '50.00'}];
+		$scope.line = $scope.giftLines[0];
+		
 		$scope.retrieveCart = function(cartId) {
 			cart.retrieve(cartId).then(function(results) {
 				$scope.cart = results;
@@ -10,6 +13,22 @@ angular.module('dssMiddlewareApp')
 		};
 		
 		$scope.proceedToCheckout = function() {
+			//do stuff
+		};
+		
+		$scope.edit = function() {
+			//do stuff
+		};
+		
+		$scope.remove = function() {
+			//do stuff
+		};
+		
+		$scope.addMoreGifts = function() {
+			//do stuff
+		};
+		
+		$scope.viewDesig = function(designation) {
 			//do stuff
 		};
 });
