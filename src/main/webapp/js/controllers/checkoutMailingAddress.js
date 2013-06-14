e98986a4f3e1069e592c81e25d7b1601d3bd29a4
Bill Randall <william.randall@cru.org>
2013-06-14 10:47:39 -0400

    Grab cart from server and use it to control the mailing address values

--- src/main/webapp/js/controllers/checkoutMailingAddress.js
+++ src/main/webapp/js/controllers/checkoutMailingAddress.js
@@ -2,41 +2,54 @@
 
 angular.module('dssMiddlewareApp')
 	.controller('CheckoutMailingAddressCtrl', function ($scope, $routeParams, $location, giftCrud, cartCrud) {
+		var params = $routeParams;
+		var cart;
+		
 		$scope.initPage = function() {
-			$scope.personalInfo = {personType: 'Household', isStaff: false,
-									mailingAddress: {countryCode: ''}};
-			$scope.prefixes = [{namePrefix: 'Mr.', namePrefixCode: 'Mr'},
-			                   {namePrefix: 'Mrs.', namePrefixCode: 'Mrs'},
-			                   {namePrefix: 'Lieutenant Commander (NAVY,CG)', namePrefixCode: 'LCDR'}];
-			$scope.suffixes = [{suffix: 'Jr.', nameSuffix: 'Jr.'},
-			                   {suffix: 'Sr.', nameSuffix: 'Sr.'},
-			                   {suffix: 'II', nameSuffix: 'II'},
-			                   {suffix: 'III', nameSuffix: 'III'},
-			                   {suffix: 'IV', nameSuffix: 'IV'}];
-			$scope.phoneTypes = [{display: 'Home', value: 'HOME'},
-			                     {display: 'Work', value: 'WORK'},
-			                     {display: 'Mobile', value: 'MOBILE'}];
-			$scope.states = [{stateCode: 'AE', stateName: 'Armed Forces Europe (includes Africa, Middle East and Canada)'},
-			                 {stateCode: 'IN', stateName: 'Indiana'},
-			                 {stateCode: 'FL', stateName: 'Florida'},
-			                 {stateCode: 'TX', stateName: 'Texas'}];
-			$scope.countries = [{countryCode: 'Canada', countryName: 'Canada'},
-			                    {countryCode: 'Macedonia, The Former Yugoslav', countryName: 'Macedonia, The Former Yugoslav'},
-			                    {countryCode: 'USA', countryName: 'USA'}, 
-			                    {countryCode: 'Zimbabwe', countryName: 'Zimbabwe'}];
-			$scope.loggedIn = false;
-			$scope.failoverMode = false;
 			
-			if($scope.personalInfo.mailingAddress.countryCode == '') {
-				$scope.personalInfo.mailingAddress.countryCode = 'USA';
-			}
+			cartCrud.retrieve(params.cartId).then(function(data) {
+				cart = data;
+				$scope.mailingAddress = cart.mailingAddress;
+				$scope.primaryName = cart.primaryName;
+				$scope.spouseName = cart.spouseName;
+				
+				//TODO: Get these from the server
+				$scope.personalInfo = {personType: 'Household', isStaff: false};
+				$scope.prefixes = [{namePrefix: 'Mr.', namePrefixCode: 'Mr'},
+				                   {namePrefix: 'Mrs.', namePrefixCode: 'Mrs'},
+				                   {namePrefix: 'Lieutenant Commander (NAVY,CG)', namePrefixCode: 'LCDR'}];
+				$scope.suffixes = [{suffix: 'Jr.', nameSuffix: 'Jr.'},
+				                   {suffix: 'Sr.', nameSuffix: 'Sr.'},
+				                   {suffix: 'II', nameSuffix: 'II'},
+				                   {suffix: 'III', nameSuffix: 'III'},
+				                   {suffix: 'IV', nameSuffix: 'IV'}];
+				$scope.phoneTypes = [{display: 'Home', value: 'HOME'},
+				                     {display: 'Work', value: 'WORK'},
+				                     {display: 'Mobile', value: 'MOBILE'}];
+				$scope.states = [{stateCode: 'AE', stateName: 'Armed Forces Europe (includes Africa, Middle East and Canada)'},
+				                 {stateCode: 'IN', stateName: 'Indiana'},
+				                 {stateCode: 'FL', stateName: 'Florida'},
+				                 {stateCode: 'TX', stateName: 'Texas'}];
+				$scope.countries = [{countryCode: 'Canada', countryName: 'Canada'},
+				                    {countryCode: 'Macedonia, The Former Yugoslav', countryName: 'Macedonia, The Former Yugoslav'},
+				                    {countryCode: 'USA', countryName: 'USA'}, 
+				                    {countryCode: 'Zimbabwe', countryName: 'Zimbabwe'}];
+				$scope.loggedIn = false;
+				$scope.failoverMode = false;
+				
+				//If blank, set country code to the default
+				if($scope.mailingAddress.countryCode == '' || $scope.mailingAddress.countryCode == null) {
+					$scope.mailingAddress.countryCode = 'USA';
+				}
+			});
+			
 		};
 		
-		$scope.isUsa = function(personalInfo) {
-			return personalInfo.mailingAddress.countryCode === 'USA';
+		$scope.isUsa = function(mailingAddress) {
+			return mailingAddress.countryCode === 'USA';
 		};
 		
-		$scope.isCanadianAddress = function(personalInfo) {
-			return personalInfo.mailingAddress.countryCode === 'Canada';
+		$scope.isCanadianAddress = function(mailingAddress) {
+			return mailingAddress.countryCode === 'Canada';
 		};
 	});
